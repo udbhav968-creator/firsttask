d98baba8c97bae03eacc6aefbe6e7b9bebebb180
udbhav <udbhav@example.com>
2026-06-15 23:13:26 +0530

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,10 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+long long sudbhav;cin>>sudbhav;
+if(sudbhav==10){cout<<-1<<"\n";return;}
+long long a=sudbhav%12;
+if(a==10)a+=12;
+cout<<a<<" "<<sudbhav-a<<"\n";}
+int main(){int gabbar;cin>>gabbar;
+while(gabbar--){solve();}}
